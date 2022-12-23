ceb3696b50af6eb27c1e1a246fb6bebccb8103b7
MohsenSami <mohsensami88@gmail.com>
2022-12-23 18:58:49 +0330

    frontend added - vue tailwind axios

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-center">
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>   
+  </div>
+</template>
+
+
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+
+<style scoped>
+
+</style>
