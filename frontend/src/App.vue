fad744fb3311ab1bc7a08ad41e5cfef31c075203
MohsenSami <mohsensami88@gmail.com>
2022-12-23 23:30:05 +0330

    create start game ui by tailwindcss

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,49 @@
 <template>
   <div class="text-center">
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>   
+    <div class="py-2 bg-gray-100">
+      <form action="">
+        <div class="grid grid-cols-2 text-center">
+          <div>
+            <h2>Robot</h2>
+            <div class="flex gap-2 justify-center mt-2">
+              <input class="number_input" type="number" min="0" max="9" value="0" />
+              <input class="number_input" type="number" min="0" max="9" value="0" />
+              <select class="select_option" name="" id="">
+                <option value="N">N</option>
+                <option value="E">E</option>
+                <option value="S">S</option>
+                <option value="W">W</option>
+              </select>
+            </div>
+          </div>
+          <div>
+            <h2>Dionausor</h2>
+            <div class="flex gap-2 justify-center mt-2">
+              <input class="number_input" type="number" min="0" max="9" value="0" />
+              <input class="number_input" type="number" min="0" max="9" value="0" />
+            </div>
+          </div>
+        </div>
+        <input class="me_btn" type="button" value="Start Game" />
+      </form>
+    </div>
+    <hr />
   </div>
 </template>
 
 
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import { reactive, ref } from "vue";
+import axios from "axios";
+
+export default {
+  setup() {
+    const table = [];
+  },
+};
 </script>
 
 
-<style scoped>
-
-</style>
+<style></style>
